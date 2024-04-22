b880116980d9128c5dbe83dadae38b8266bccc71
omeher26 <onkarmeher500b@gmail.com>
2024-04-23 03:10:32 +0530

    edit

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -13,7 +13,8 @@ export const uploadImage = async (request,response) => {
     try{
         const file = await File.create(fileObj);
         // response.status(200).json({ path : `http://localhost:8000/file/${file._id}`})
-        response.status(200).json({ path : `http://localhost:${process.env.PORT}/file/${file._id}`})
+        // response.status(200).json({ path : `http://localhost:${process.env.PORT}/file/${file._id}`})
+        response.status(200).json({ path : `${process.env.LIVE_URL}/file/${file._id}`})
     } catch (error) {
         console.error(error.message);
         response.status(500).json({ error: error.message })
